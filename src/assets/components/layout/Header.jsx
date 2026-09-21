e9df7e358bea8850fb76af2cce384c30076cfa21
ilfosch18-dot <ilfosch18@gmail.com>
2026-09-21 16:57:44 +0200

    header style

--- src/assets/components/layout/Header.jsx
+++ src/assets/components/layout/Header.jsx
@@ -2,40 +2,20 @@ import logo from '../../img/marvel-logo.png';
 
 export default function Header() {
   return (
-    <header>
-        <img src={logo} alt="Logo Marvel"/>
-        <ul>
-        <li>
-            Characters
-        </li>        
-        <li>
-            Comics
-        </li>      
-          <li>
-            Movies
-        </li>        
-        <li>
-            Tv
-        </li>       
-         <li>
-            Games
-        </li>       
-         <li>
-            Collectibles
-        </li>       
-         <li>
-            Videos
-        </li>       
-         <li>
-            Fans
-        </li>       
-         <li>
-            News
-        </li>
-        <li>
-            Shop
-        </li>
+    <header className="flex items-center justify-evenly">
+      <img src={logo} alt="Logo Marvel" />
+        <ul className="flex items-center gap-6 text-sm uppercase">
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Collectibles</li>
+          <li>Videos</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
         </ul>
     </header>
-  )
+  );
 }
