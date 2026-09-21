8e3569b2453e608d96736c366d3b70a03c30049e
ilfosch18-dot <ilfosch18@gmail.com>
2026-09-21 16:13:00 +0200

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/components/layout/Header"
+import Main from "./assets/components/layout/Main"
+import Footer from "./assets/components/layout/Footer"
+
+function App() {
+  return (
+    <>
+<Header />
+<Main />
+<Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/layout/Header.jsx
@@ -0,0 +1,38 @@
+export default function Header() {
+  return (
+    <header>Logo
+        <ul>
+        <li>
+            Characters
+        </li>        
+        <li>
+            Comics
+        </li>      
+          <li>
+            Movies
+        </li>        
+        <li>
+            Tv
+        </li>       
+         <li>
+            Games
+        </li>       
+         <li>
+            Collectibles
+        </li>       
+         <li>
+            Videos
+        </li>       
+         <li>
+            Fans
+        </li>       
+         <li>
+            News
+        </li>
+        <li>
+            Shop
+        </li>
+        </ul>
+    </header>
+  )
+}
